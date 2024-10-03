400a01d0a2c9edf661d3dc00e9e770a9bc212165
samir-27 <sumrasamir27@gmail.com>
2024-10-03 20:09:32 +0530

    login & register added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from "react";
 import Navbar from "./components/Navabar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
+  const location = useLocation();
   return (
-    <Router>
-      <Navbar />
+    <>
+      {location.pathname !== "/login" && location.pathname !== "/register" && (
+        <Navbar />
+      )}
+
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/about" element={<About />} />
-      
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import img from '../assets/login.webp'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    return (
+        <div>
+            <div className='relative w-screen h-screen'>
+                <div className='absolute w-full h-full bg-black opacity-60'></div>
+                <img src={img} alt="" className='h-full w-full object-cover' />
+            </div>
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                <div className="bg-white p-8 rounded-lg w-96 mx-5">
+                    <h2 className="text-2xl font-semibold mb-6 text-center">Login</h2>
+                    <form>
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block text-gray-700">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block text-gray-700">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="password" className="block text-gray-700">Login As</label>
+                            <select className='w-full px-3 py-2 border border-gray-300 rounded-md'>
+                                <option value="User">User</option>
+                                <option value="Admin">Admin</option>
+                            </select>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-sky-900 text-white py-2 px-4 rounded-md hover:bg-rose-600"
+                        >
+                            Login
+                        </button>
+                        <p className='text-sm text-gray-700 font-semibold py-3'>
+                            Haven't Created Account? <Link to="/register" className="text-blue-500 hover:underline">Register</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
