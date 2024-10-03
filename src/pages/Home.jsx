77d6414360c75478c5a6e332743356106ba7235b
samir-27 <sumrasamir27@gmail.com>
2024-10-03 10:25:13 +0530

    refactor code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import TopProducts from '../components/TopProducts'
 const Home = () => {
   return (
-    <div className=''>
+    <div className='container mx-auto xl:px-40 lg:px-28 md:px-10 px-5'>
       <Hero />
+       <TopProducts />
     </div>
   )
 }
--- src/utils/allproduct.js
+++ src/utils/allproduct.js
@@ -9,6 +9,7 @@ const allProducts = [
         category: "Sneaker",
         gender: "Men",
         rating: 4.5,
+        "purchases": 150,
         images:[
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/617852db-ae30-49e7-a6a7-ac5113cd694f/NIKE+COURT+VISION+LO+NN.png",
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/48ab3c75-71ae-42e7-adb4-0485d9e7753a/NIKE+COURT+VISION+LO+NN.png",
@@ -25,6 +26,7 @@ const allProducts = [
         category: "Sports",
         gender: "Women",
         rating: 4.8,
+        "purchases": 10,
         images:[
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4828e2bc-626e-4563-8fa8-aa68c1baa304/W+BLAZER+LOW+PLATFORM.png",
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9139fc52-413b-4d15-9227-e27e91e739fa/W+BLAZER+LOW+PLATFORM.png",
@@ -40,6 +42,7 @@ const allProducts = [
         brand: "Puma",
         category: "Sneaker",
         gender: "Unisex",
+        "purchases": 50,
         rating: 4.2,
         images:[
             "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/399844/02/fnd/IND/fmt/png/PUMA-Court-Shatter-Low-Unisex-Sneakers",
@@ -56,6 +59,7 @@ const allProducts = [
         brand: "Bata",
         category: "Casual",
         gender: "Men",
+        "purchases": 10,
         rating: 4.0,
         images:["https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dwcb639ae1/images/large/8514052_3.jpeg?sw=817",
             "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dw0e2a8a2f/images/large/8514052_5.jpeg?sw=817",
@@ -71,6 +75,7 @@ const allProducts = [
         brand: "Adidas",
         category: "Sports",
         gender: "Unisex",
+        "purchases": 250,
         rating: 4.9,
         images:[
             "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ff0654f4089f4413baa7aae700d2a08c_9366/Superstar_Shoes_Black_EG4959_02_standard_hover_hover.jpg",
@@ -86,6 +91,7 @@ const allProducts = [
         brand: "Bata",
         category: "Casual ",
         gender: "Women",
+        "purchases": 30,
         rating: 4.3,
         images:[ "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dw96d9cef3/images/large/5592721_5.jpeg?sw=817",
         "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dwa83c3250/images/large/5592721_2.jpeg?sw=817",
@@ -101,6 +107,7 @@ const allProducts = [
         category: "Sports",
         gender: "Men",
         rating: 4.6,
+        "purchases": 40,
         images:[
             "https://imagescdn.reebok.in/img/app/product/8/801740-9474587.jpg?auto=format&w=390",
             "https://imagescdn.reebok.in/img/app/product/8/801740-9474591.jpg?auto=format&w=390",
@@ -117,6 +124,7 @@ const allProducts = [
         category: "Sports",
         gender: "Kids",
         rating: 4.1,
+        "purchases": 76,
         images:[
             "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes",
             "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/sv04/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes",
@@ -125,13 +133,14 @@ const allProducts = [
     },
     {
         id: 10,
-        name: "Nike Court Vision Low Next Nature",
+        title: "Nike Court Vision Low Next Nature",
         desc:"An iconic look that lasts. This AJ1 pairs the classic design of the original with premium materials that will keep you going all day.",
         path: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4284894e-e57e-4d95-ae7b-1e8fcc9eeb9a/NIKE+COURT+VISION+LO+NN.png",
         price: " 4995.00",
         brand: "Nike",
         category: "Sneaker",
         gender: "Men",
+        "purchases": 100,
         rating: 4.5,
         images:[
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/617852db-ae30-49e7-a6a7-ac5113cd694f/NIKE+COURT+VISION+LO+NN.png",
@@ -148,6 +157,7 @@ const allProducts = [
         brand: "Nike",
         category: "Sports",
         gender: "Women",
+        "purchases": 112,
         rating: 4.8,
         images:[
             "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4828e2bc-626e-4563-8fa8-aa68c1baa304/W+BLAZER+LOW+PLATFORM.png",
@@ -165,6 +175,7 @@ const allProducts = [
         category: "Sneaker",
         gender: "Unisex",
         rating: 4.2,
+        "purchases": 86,
         images:[
             "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/399844/02/fnd/IND/fmt/png/PUMA-Court-Shatter-Low-Unisex-Sneakers",
             "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/399844/02/bv/fnd/IND/fmt/png/PUMA-Court-Shatter-Low-Unisex-Sneakers",
@@ -181,6 +192,7 @@ const allProducts = [
         category: "Casual",
         gender: "Men",
         rating: 4.0,
+        "purchases": 5,
         images:["https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dwcb639ae1/images/large/8514052_3.jpeg?sw=817",
             "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dw0e2a8a2f/images/large/8514052_5.jpeg?sw=817",
             "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dwe95b2375/images/large/8514052_6.jpeg?sw=817"
@@ -196,6 +208,7 @@ const allProducts = [
         category: "Sports",
         gender: "Unisex",
         rating: 4.9,
+        "purchases": 15,
         images:[
             "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ff0654f4089f4413baa7aae700d2a08c_9366/Superstar_Shoes_Black_EG4959_02_standard_hover_hover.jpg",
             "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/cfc151d900394855a485aae700d2d84b_9366/Superstar_Shoes_Black_EG4959_42_detail.jpg",
@@ -211,43 +224,11 @@ const allProducts = [
         category: "Casual ",
         gender: "Women",
         rating: 4.3,
+        "purchases": 180,
         images:[ "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dw96d9cef3/images/large/5592721_5.jpeg?sw=817",
         "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dwa83c3250/images/large/5592721_2.jpeg?sw=817",
         "https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-in-master-catalog/default/dw29c71f7a/images/large/5592721_4.jpeg?sw=817"] 
-    },
-    {
-        id: 16,
-        title: "Reebok Men Walking Shoes",
-        path: "https://imagescdn.reebok.in/img/app/product/8/801740-9474590.jpg?auto=format&w=390",
-        desc:"Experience comfort and style with Reebok`s black walking shoes for men. These shoes are designed using high-quality, breathable mesh material for maximum comfort. With synthetic soles, your feet stay protected, promoting free movement and stability",
-        price: "6500",
-        brand: "Reebok",
-        category: "Sports",
-        gender: "Men",
-        rating: 4.6,
-        images:[
-            "https://imagescdn.reebok.in/img/app/product/8/801740-9474587.jpg?auto=format&w=390",
-            "https://imagescdn.reebok.in/img/app/product/8/801740-9474591.jpg?auto=format&w=390",
-            "https://imagescdn.reebok.in/img/app/product/8/801740-9474585.jpg?auto=format&w=390"
-        ]
-    },
-    {
-        id: 17,
-        title: "TORI Bubble Bloom",
-        path: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/sv01/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes",
-        desc:"Unleash the inner artist in your little one with the playful TORI Bubble Bloom. Bright floral accents and soft satin laces add fun to every step,",
-        price: "2500",
-        brand: "Puma",
-        category: "Sports",
-        gender: "Kids",
-        rating: 4.1,
-        images:[
-            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes",
-            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/sv04/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes",
-            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/401290/03/bv/fnd/IND/fmt/png/TORI-Bubble-Bloom-Youth-Shoes"
-        ]
     }
-
     
     
     
